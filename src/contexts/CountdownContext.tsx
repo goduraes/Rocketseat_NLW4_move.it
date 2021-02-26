232c0166cd7df056b76ac7ff78cccb3c840117e7
Gabriel Durães <gabriel.duraes@usemobile.xyz>
2021-02-26 15:55:34 -0300

    move.it

---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,73 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CountdownContextData {
+  minutes: number;
+  seconds: number;
+  hasFinished: boolean;
+  isActive: boolean;
+  startCountdown: () => void;
+  resetCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+  children: ReactNode
+}
+
+export const CountdownContext = createContext({
+
+} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  //arredonda os minutos
+  const minutes = Math.floor(time / 60);
+  //pega o resto dos minutos que são os segundos
+  const seconds = time % 60;
+
+  //inicia o contador
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(0.1 * 60);
+  }
+
+  //executa algo assim que as variaveis active e time são alteradas
+  useEffect(() => {
+    //valida se o contador esta ativo e se o time não esta zerado
+    if(isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, time])
+
+  return (
+    <CountdownContext.Provider value={{
+      minutes,
+      seconds,
+      hasFinished,
+      isActive,
+      startCountdown,
+      resetCountdown
+    }}>
+      {children}
+    </CountdownContext.Provider>
+  )
+}
